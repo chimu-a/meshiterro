2978265f3de2943efd6483522654500de6e70e43
chimu-a <138343606+chimu-a@users.noreply.github.com>
2023-07-18 01:26:56 +0000

    [finish]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
   devise_for :users
